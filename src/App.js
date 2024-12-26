9e9211aaeee173322fb039ffcf75620ccb30affb
Rodrigo Graça <rodrigomgraca@ua.pt>
2024-12-26 16:31:52 +0000

    try put tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native";
 
+
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center bg-blue-500">
-      <Text className="text-red text-lg font-bold">Tailwind está funcionando!</Text>
+      <Text className="text-red-500 text-lg font-bold">Tailwind está funcionando!</Text>
     </View>
   );
 }
