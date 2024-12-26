8ced8fb156f82dd1554078bd9b5744d22a4eadb9
Rodrigo Graça <rodrigomgraca@ua.pt>
2024-12-26 16:51:55 +0000

    cores no tailwind

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-blue-500">
-      <Text className="text-white text-lg font-bold">Hello NativeWind</Text>
+    <View className="flex-1 justify-center items-center bg-yellow">
+      <Text className="text-xl font-bold text-center">
+        Olá!{"\n"}Bem-Vindo à{"\n"}LUMICHECK!
+      </Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "orange": "#ff9d00",
+        "yellow": "#fcc766",
+        "light-yellow": "#ffe5b4",
+        "off-white": "#fff9ef",
+        "violet": "#aa82da",
+        "dark-gray": "#686868",
+      },
+    },
   },
   plugins: [],
 }
