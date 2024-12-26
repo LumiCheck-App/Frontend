13bc6d2a14ac307b2b42fb8ce91a72f5c0755d87
Rodrigo Graça <rodrigomgraca@ua.pt>
2024-12-26 16:41:12 +0000

    tailwind funciona

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { View, Text } from "react-native";
-
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center bg-blue-500">
-      <Text className="text-red-500 text-lg font-bold">Tailwind está funcionando!</Text>
+      <Text className="text-white text-lg font-bold">Hello NativeWind</Text>
     </View>
   );
 }
