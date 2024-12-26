12953e4bb4f9449acb08215b81e156f98f667c00
Rodrigo Graça <rodrigomgraca@ua.pt>
2024-12-26 15:53:32 +0000

    create app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-500">
+      <Text className="text-red text-lg font-bold">Tailwind está funcionando!</Text>
+    </View>
+  );
+}
