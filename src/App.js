fbe59dd72e3bedd4d3e75c181010e96df48e060a
Rodrigo Graça <rodrigomgraca@ua.pt>
2024-12-28 16:09:51 +0000

    hsomepage quase feita

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -30,7 +30,7 @@ export default function App() {
 
   if (!fontsLoaded) {
     return (
-      <View className="flex-1 justify-center items-center bg-gray-100">
+      <View className="flex-1 justify-center items-center bg-off'white">
         <Text>Carregando fontes...</Text>
       </View>
     );
@@ -46,14 +46,14 @@ export default function App() {
         screenOptions={({ route }) => ({
           headerShown: false,
           tabBarShowLabel: false,
-          // tabBarStyle: {
-          //   paddingBottom: 15, // Espaçamento inferior dentro da barra
-          //   paddingTop: 10, // Espaçamento superior dentro da barra
-          //   backgroundColor: "#fff", // Cor de fundo da barra
-          // },
+          tabBarStyle: {
+            paddingTop: 5, // Espaçamento superior dentro da barra
+            paddingBottom: 5, // Espaçamento inferior dentro da barra
+            backgroundColor: "#fff", // Cor de fundo da barra
+          },
           tabBarIcon: ({ focused, size }) => {
             let IconComponent;
-            let color = focused ? "#fcc766" : "#d0d0d0"; // Define as cores: ativo e inativo
+            let color = focused ? "#fcc766" : "#d0d0d0";
 
             switch (route.name) {
               case "Home":
@@ -92,26 +92,29 @@ export default function App() {
 
   // Stack Navigator (para gerenciar Welcome e HomeTabs)
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {/* Tela de boas-vindas sem navbar */}
-        <Stack.Screen
-          name="Welcome"
-          component={WelcomePage}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={LoginPage}
-          options={{ headerShown: false }}
-        />
-        {/* Tela HomeTabs com navbar */}
-        <Stack.Screen
-          name="HomeTabs"
-          component={HomeTabs}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* Tela de boas-vindas sem navbar */}
+          <Stack.Screen
+            name="Welcome"
+            component={WelcomePage}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={LoginPage}
+            options={{ headerShown: false }}
+          />
+          {/* Tela HomeTabs com navbar */}
+          <Stack.Screen
+            name="HomeTabs"
+            component={HomeTabs}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,86 +1,86 @@
 import React from "react";
 import { Text, View, Image, TouchableOpacity } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { SvgUri } from "react-native-svg";
+
+// Importar os ícones SVG diretamente
+import TrophyGoldIcon from "../../assets/icons/trophygold.svg";
+import QuestionIcon from "../../assets/icons/question.svg";
+import CheckIcon from "../../assets/icons/check.svg";
 
 export default function HomePage() {
     return (
         <LinearGradient
             colors={["#ffe5b4", "#fff9ef", "#fff9ef"]}
-            locations={[0, 0.3, 1]}
-            className="flex-1"
+            locations={[0, 0.5, 1]}
+            style={{ flex: 1 }}
         >
-            <View className="flex-1 items-center pt-12">
-                {/* Ícone PNG */}
-                <Image
-                    source={require("../../assets/Lumi.png")} // Caminho do ícone grande (fogo)
-                    className="w-20 h-20"
-                    resizeMode="contain"
-                />
+            <View className="flex-1 py-8 px-4">
 
-                {/* Texto de boas-vindas */}
-                <Text className="text-2xl font-bold text-gray-800 mt-2">
-                    Olá, Rodrigo!
-                </Text>
+                <View className="flex-1">
+                    {/* Secção de Ícones e Pontos */}
+                    <View className="absolute top-8 right-5 items-end">
+                        {/* Ícone de "?" */}
+                        <View className="flex-row items-center mb-2">
+                            <Text className="text-lg font-bold mr-2">14</Text>
+                            <QuestionIcon width={24} height={24} />
+                        </View>
 
-                {/* Botão de Monitorização */}
-                <TouchableOpacity className="bg-yellow-500 rounded-full py-2 px-6 mt-5">
-                    <Text className="text-white font-bold text-base">Começar Monitorização</Text>
-                </TouchableOpacity>
-
-                {/* Secção de Tarefas Diárias */}
-                <View className="w-11/12 bg-white rounded-lg p-4 mt-8 shadow">
-                    <Text className="text-lg font-bold text-gray-600 mb-4">
-                        Tarefas Diárias
-                    </Text>
-
-                    {/* Primeira tarefa */}
-                    <View className="flex-row items-center justify-between mb-3">
-                        <Text className="text-sm text-gray-600 flex-1">
-                            Estar apenas 2 horas no Insta hoje
-                        </Text>
-                        <SvgUri
-                            width={24}
-                            height={24}
-                            uri="../../assets/icons/check.svg" // Caminho do ícone SVG
-                        />
+                        {/* Ícone de troféu */}
+                        <View className="flex-row items-center">
+                            <Text className="text-lg font-bold mr-2">2</Text>
+                            <TrophyGoldIcon width={24} height={24} />
+                        </View>
                     </View>
 
-                    {/* Segunda tarefa */}
-                    <View className="flex-row items-center justify-between">
-                        <Text className="text-sm text-gray-600 flex-1">
-                            Falar com os amigos
-                        </Text>
-                        <SvgUri
-                            width={24}
-                            height={24}
-                            uri="../..assets/icons/check.svg" // Caminho do ícone SVG
+
+                    <View className="flex-1 items-center pt-12">
+                        {/* Ícone PNG */}
+                        <Image
+                            source={require("../../assets/Lumi.png")} // Caminho do ícone grande (fogo)
+                            className="w-40 h-40"
+                            resizeMode="contain"
                         />
+
+                        {/* Texto de boas-vindas */}
+                        <Text className="text-2xl font-bold text-gray-800 mt-6">
+                            Olá, Rodrigo!
+                        </Text>
+
+                        {/* Botão de Monitorização */}
+                        <TouchableOpacity className="bg-yellow rounded-lg w-11/12 py-3 mt-12 items-center">
+                            <Text className="text-xl text-white font-bold">Começar Monitorização</Text>
+                        </TouchableOpacity>
+
+                        {/* Secção de Tarefas Diárias */}
+                        <View className="w-11/12 mt-8">
+                            {/* Cabeçalho da seção */}
+                            <View className="flex-row items-center justify-between mb-4">
+                                <Text className="text-xl font-bold text-black">Tarefas Diárias</Text>
+                                <Text className="text-md font-bold text-orange self-end">7 HORAS</Text>
+                            </View>
+
+                            {/* Primeira tarefa */}
+                            <View className="flex-row items-center justify-between bg-white rounded-lg px-4 mb-2 border border-light-gray">
+                                <Text className="text-lg text-gray-600 flex-1 py-3">
+                                    Estar apenas 2 horas no Insta hoje
+                                </Text>
+                                <View className="h-full w-[1px] bg-light-gray mr-4" />
+                                <CheckIcon width={24} height={24} />
+                            </View>
+
+                            {/* Segunda tarefa */}
+                            <View className="flex-row items-center justify-between bg-white rounded-lg px-4 border border-light-gray">
+                                <Text className="text-lg text-gray-600 flex-1 py-3">
+                                    Falar com os amigos
+                                </Text>
+                                <View className="h-full w-[1px] bg-light-gray mr-4" />
+                                <CheckIcon width={24} height={24} style={{ opacity: 0 }} />
+                            </View>
+                        </View>
+
                     </View>
                 </View>
 
-                {/* Secção de Ícones e Pontos */}
-                <View className="flex-row justify-between items-center w-28 mt-8">
-                    {/* Ícone de "?" */}
-                    <View className="items-center">
-                        <SvgUri
-                            width={24}
-                            height={24}
-                            uri="../../assets/icons/question.svg" // Caminho do ícone SVG
-                        />
-                        <Text className="text-sm font-bold text-gray-600 mt-1">14</Text>
-                    </View>
-
-                    {/* Ícone de troféu */}
-                    <View className="items-center">
-                        <SvgUri
-                            width={24}
-                            height={24}
-                            uri="../../assets/icons/trophygold.svg" // Caminho do ícone SVG
-                        />
-                        <Text className="text-sm font-bold text-gray-600 mt-1">2</Text>
-                    </View>
-                </View>
             </View>
         </LinearGradient>
     );
