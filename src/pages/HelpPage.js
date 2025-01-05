e7d4d3862fe1405e2e40edf667790aa08c241617
Rodrigo Graça <rodrigomgraca@ua.pt>
2025-01-05 13:03:08 +0000

    mapa e contactos a funcionar

--- src/pages/HelpPage.js
+++ src/pages/HelpPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Alert, TouchableOpacity, FlatList, Modal } from "react-native";
+import { View, Text, Alert, TouchableOpacity, FlatList, Modal, ScrollView } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { LinearGradient } from "expo-linear-gradient";
 import * as Location from "expo-location";
@@ -26,7 +26,6 @@ const distritos = [
     "Viseu",
     "Açores",
     "Madeira",
-    "Outros",
 ];
 
 export default function HelpPage() {
@@ -64,12 +63,16 @@ export default function HelpPage() {
     }, []);
 
     const filteredMarkers = selectedDistrito
-        ? markersOnMap.filter((marker) => {
-            const markerDistrito = marker.distrito?.toLowerCase();
-            const selectedDistritoLower = selectedDistrito.toLowerCase();
-            console.log(`Comparando: ${markerDistrito} === ${selectedDistritoLower}`);
-            return markerDistrito === selectedDistritoLower;
-        })
+        ? selectedDistrito === "Outros"
+            ? markersOnMap.filter(
+                (marker) => !distritos.some((distrito) =>
+                    marker.Distrito?.toLowerCase() === distrito.toLowerCase()
+                )
+            )
+            : markersOnMap.filter(
+                (marker) =>
+                    marker.Distrito?.toLowerCase() === selectedDistrito.toLowerCase()
+            )
         : [];
 
     return (
@@ -78,102 +81,113 @@ export default function HelpPage() {
             locations={[0, 0.5, 1]}
             style={{ flex: 1 }}
         >
-            <View style={{ flex: 1 }}>
-                {/* Mapa */}
-                <MapView
-                    style={{
-                        width: "100%", // Largura igual a 100% do contêiner pai
-                        aspectRatio: 1, // Define a proporção de altura para largura (1:1)
-                    }}
-                    region={region}>
-                    {/* Marcador do usuário */}
-                    {location && (
-                        <Marker
-                            coordinate={location}
-                            title="Você"
-                            image={require('../../assets/Lumi-Mapa.png')}
-                        />
-                    )}
-
-
-                    {/* Renderiza os marcadores */}
-                    {markersOnMap.map((marker, index) => {
-                        const { LatLng, placeName, placeDesc } = marker;
-                        if (!LatLng || LatLng.length === 0) return null;
-
-                        const { lat, lng } = LatLng[0];
-                        return (
+            <ScrollView>
+                <View style={{ flex: 1 }}>
+                    {/* Mapa */}
+                    <MapView
+                        style={{
+                            width: "100%",
+                            aspectRatio: 1,
+                        }}
+                        region={region}>
+                        {/* Marcador do usuário */}
+                        {location && (
                             <Marker
-                                key={index}
-                                coordinate={{ latitude: lat, longitude: lng }}
-                                title={placeName.replace(/<[^>]+>/g, "")}
-                                description={placeDesc.replace(/<[^>]+>/g, "")}
+                                coordinate={location}
+                                title="Você"
+                                image={require('../../assets/Lumi-Mapa.png')}
                             />
-                        );
-                    })}
-                </MapView>
-
-                <View className="flex-1 px-4 items-center">
-                    <View className="w-11/12 mt-8">
-                        <TouchableOpacity
-                            className="p-3 bg-[#ffe5b4] rounded-md items-center"
-                            onPress={() => setDropdownVisible(true)}
-                        >
-                            <Text className="text-lg font-bold text-gray-800">
-                                {selectedDistrito || "Selecione um Distrito"}
-                            </Text>
-                        </TouchableOpacity>
-                        <Modal
-                            visible={dropdownVisible}
-                            transparent={true}
-                            animationType="slide"
-                        >
-                            <View className="flex-1 bg-black bg-opacity-50 justify-center">
-                                <FlatList
-                                    data={distritos}
-                                    keyExtractor={(item, index) => index.toString()}
-                                    renderItem={({ item }) => (
-                                        <TouchableOpacity
-                                            className="p-4 bg-white border-b border-gray-300"
-                                            onPress={() => {
-                                                setSelectedDistrito(item);
-                                                setDropdownVisible(false);
-                                            }}
-                                        >
-                                            <Text className="text-lg text-gray-800">{item}</Text>
-                                        </TouchableOpacity>
-                                    )}
-                                />
-                            </View>
-                        </Modal>
-                    </View>
-
-                    {/* Lista de Contactos */}
-                    <View className="w-11/12 mt-8">
-                        <Text className="text-xl font-bold mb-2">Contactos</Text>
-                        {selectedDistrito && filteredMarkers.length > 0 ? (
-                            filteredMarkers.map((marker, index) => (
-                                <View key={index} className="mb-4 p-3 bg-white rounded-md">
-                                    <Text className="text-lg font-bold">
-                                        {marker.placeName.replace(/<[^>]+>/g, "")}
-                                    </Text>
-                                    <Text className="text-sm text-gray-600">
-                                        {marker.placeDesc.replace(/<[^>]+>/g, "")}
-                                    </Text>
-                                </View>
-                            ))
-                        ) : selectedDistrito ? (
-                            <Text className="text-center text-gray-500">
-                                Nenhum psicólogo encontrado no distrito selecionado.
-                            </Text>
-                        ) : (
-                            <Text className="text-center text-gray-500">
-                                Selecione um distrito para ver os contactos disponíveis.
-                            </Text>
                         )}
+
+                        {/* Renderiza os marcadores */}
+                        {markersOnMap.map((marker, index) => {
+                            const { Coords, Nome, Descricao } = marker;
+                            if (!Coords || Coords.length === 0) return null;
+
+                            const { lat, lng } = Coords[0];
+                            return (
+                                <Marker
+                                    key={index}
+                                    coordinate={{ latitude: lat, longitude: lng }}
+                                    title={Nome.replace(/<[^>]+>/g, "")}
+                                    description={Descricao.replace(/<[^>]+>/g, "")}
+                                />
+                            );
+                        })}
+                    </MapView>
+
+                    <View className="flex-1 px-4 items-center">
+                        <View className="w-11/12 mt-8">
+                            <TouchableOpacity
+                                className="p-3 bg-[#ffe5b4] rounded-md items-center"
+                                onPress={() => setDropdownVisible(true)}
+                            >
+                                <Text className="text-lg font-bold">
+                                    {selectedDistrito || "Selecione um Distrito"}
+                                </Text>
+                            </TouchableOpacity>
+                            <Modal
+                                visible={dropdownVisible}
+                                transparent={true}
+                                animationType="slide"
+                            >
+                                <View className="flex-1 justify-end">
+                                    {/* Fundo transparente do modal */}
+                                    <TouchableOpacity
+                                        className="flex-1"
+                                        activeOpacity={1}
+                                        onPress={() => setDropdownVisible(false)}
+                                    />
+
+                                    {/* Parte inferior do modal */}
+                                    <View className="h-1/2 bg-white rounded-t-lg">
+                                        <FlatList
+                                            data={[...distritos, "Outros"]}
+                                            keyExtractor={(item, index) => index.toString()}
+                                            renderItem={({ item }) => (
+                                                <TouchableOpacity
+                                                    className="p-4 border-b border-gray-300"
+                                                    onPress={() => {
+                                                        setSelectedDistrito(item);
+                                                        setDropdownVisible(false);
+                                                    }}
+                                                >
+                                                    <Text className="text-lg text-gray-800">{item}</Text>
+                                                </TouchableOpacity>
+                                            )}
+                                        />
+                                    </View>
+                                </View>
+                            </Modal>
+                        </View>
+
+                        {/* Lista de Contactos */}
+                        <View className="w-11/12 mt-8">
+                            <Text className="text-xl font-bold mb-2">Contactos</Text>
+                            {selectedDistrito && filteredMarkers.length > 0 ? (
+                                filteredMarkers.map((marker, index) => (
+                                    <View key={index} className="mb-4 p-3 bg-white rounded-md">
+                                        <Text className="text-lg font-bold">
+                                            {marker.Nome.replace(/<[^>]+>/g, "")}
+                                        </Text>
+                                        <Text className="text-sm text-gray-600">
+                                            {marker.Descricao.replace(/<[^>]+>/g, "")}
+                                        </Text>
+                                    </View>
+                                ))
+                            ) : selectedDistrito ? (
+                                <Text className="text-center text-gray-500">
+                                    Nenhum psicólogo encontrado no distrito selecionado.
+                                </Text>
+                            ) : (
+                                <Text className="text-center text-gray-500">
+                                    Selecione um distrito para ver os contactos disponíveis.
+                                </Text>
+                            )}
+                        </View>
                     </View>
                 </View>
-            </View>
+            </ScrollView>
         </LinearGradient>
     );
 }
